649c19ce8adc6d4755f7304901a9deea07b6885a
adam.czajkowski <adam.czajkowski@g2a.com>
2024-06-03 11:22:52 +0200

    lesson 1 - FizzBuzz

--- app/src/main/java/com/project/kotlin_challange/lesson_1/Main.kt
+++ app/src/main/java/com/project/kotlin_challange/lesson_1/Main.kt
@@ -1,5 +1,21 @@
 package com.project.kotlin_challange.lesson_1
 
-    fun main() {
-        println("Hello world")
+/**
+ * FizzBuzz
+ *
+ * Napisz program, który wypisuje liczby od 1 do 100.
+ * Jednak dla liczb podzielnych przez 3 zamiast liczby wypisz słowo "Kajko",
+ * dla liczb podzielnych przez 5 wypisz "Kokosz",
+ * a dla liczb podzielnych zarówno przez 3 jak i 5 wypisz "Kajko i Kokosz".
+ */
+
+fun main() {
+    for(i in 1..100) {
+        when {
+            i % 3 == 0 && i % 5 != 0 -> println("Kajko")
+            i % 5 == 0 && i % 3 != 0 -> println("Kokosz")
+            i % 5 == 0 && i % 3 == 0 -> println("Kajko i Kokosz")
+            else -> println(i)
+        }
     }
+}
